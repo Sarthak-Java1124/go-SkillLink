8ce5641b1804342be2c1fccdf4476d21f2e4f2c7
Sarthak-Java1124 <sarthak@fensory.com>
2025-11-03 00:51:39 +0530

    feat : dbConnect made

---
+++ utils/dbConnect.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func main() {
+	uri := "mongodb://localhost:27017/skilllink"
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		log.Fatal("Error occured in connecting with database", err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+}
